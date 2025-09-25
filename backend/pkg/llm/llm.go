502adcfd464a7226cbdd593416fe3a322a4c0d18
dedfaf <dedfaf3@gmail.com>
2025-09-25 17:32:45 +0800

    Select latest messages from a session based on the token limit.

--- backend/pkg/llm/llm.go
+++ backend/pkg/llm/llm.go
@@ -6,9 +6,27 @@ import (
 )
 
 // Select latest messages from a session based on the token limit.
-func SelectMessage(session storage.Session, tokenLimit int) ([]storage.Message, error) {
-	// TODO
+// the input session should contain the latest user input, in order to make the calculated tokens contains the latest user input.
+func SelectMessage(session storage.Session, tokenLimit int, model Model) ([]storage.Message, error) {
 	var selectedMessages []storage.Message
+	// return selectedMessages, nil
+
+	sumToken := 0
+	// for message := range session.Messages
+	for i := len(session.Messages) - 1; i >= 0; i-- {
+		msg := session.Messages[i]
+		token, err := GetStringToken(msg.Content, model)
+		if err != nil {
+			return nil, err
+		}
+		if sumToken+token >= tokenLimit {
+			// if the sum of tokens exceeds the limit, break
+			break
+		} else {
+			selectedMessages = append([]storage.Message{msg}, selectedMessages...) // revert append
+			sumToken += token
+		}
+	}
 	return selectedMessages, nil
 }
 
